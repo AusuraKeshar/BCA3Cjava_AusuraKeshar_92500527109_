af2d509cdd85a33977316b5d0d4cca71446c72ac
Ausura Keshar <kesharbhaigadhvi@gmail.com>
2026-07-10 10:37:55 +0530

    Update program4.java

--- program4.java
+++ program4.java
@@ -1,45 +1,20 @@
+/*write a java program to perform different arithmatic operations(using command line Args)*/
 class program4
- {
-    public static void main(String args[]) {
+     {
 
-        // Check if 3 arguments are provided
-        if (args.length != 3) {
-            System.out.println("Usage: java ArithmeticOperation <num1> <operator> <num2>");
-            return;
-        }
+	public static void main(String args[])
+     {
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
 
-        // Convert command-line arguments to numbers
-        double num1 = Double.parseDouble(args[0]);
-        String operator = args[1];
-        double num2 = Double.parseDouble(args[2]);
-
-        // Perform operation
-        switch (operator) {
-            case "+":
-                System.out.println("Addition = " + (num1 + num2));
-                break;
-
-            case "-":
-                System.out.println("Subtraction = " + (num1 - num2));
-                break;
-
-            case "*":
-                System.out.println("Multiplication = " + (num1 * num2));
-                break;
-
-            case "/":
-                if (num2 != 0)
-                    System.out.println("Division = " + (num1 / num2));
-                else
-                    System.out.println("Error: Division by zero is not allowed.");
-                break;
-
-            case "%":
-                System.out.println("Modulus = " + (num1 % num2));
-                break;
-
-            default:
-                System.out.println("Invalid operator! Use +, -, *, /, or %.");
-        }
-    }
-}
+	System.out.println("first number:"+a);
+	System.out.println("second number:"+b);
+	
+	System.out.println("Addition is: "+(a+b));
+	System.out.println("Subatraction is: "+(a-b));
+	System.out.println("multiplication is: "+(a*b));
+	System.out.println("division is: "+(a/b));
+	System.out.println("modulus is: "+(a%b));
+	
+     }
+     }
