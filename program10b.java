a9f57cb86730133d581561ae71d12c2e677a53dd
Ausura Keshar <kesharbhaigadhvi@gmail.com>
2026-08-03 14:53:22 +0530

    Add files via upload

---
+++ program10b.java
@@ -0,0 +1,18 @@
+/* WRITE A JAVA PROGRAM OF 1D ARRAY AND TAKE VALUES MANUALLY. */
+
+class program10b
+{
+    public static void main(String args[])
+    {
+        int marks[] = {60, 50, 40, 30, 80};
+
+        int i;
+
+        System.out.println("MANUALLY ASSIGNED ARRAY ELEMENTS:");
+
+        for(i = 0; i < marks.length; i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }
+}
